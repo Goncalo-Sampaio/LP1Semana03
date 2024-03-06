ed08811bc44634f154eab8ba587a2150d1e8d5e3
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 15:37:33 +0000

    Creating A and b matrix and assigning the arguments, ArrayMult project

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -6,7 +6,17 @@ namespace ArrayMult
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //creating a new 2x2 matrix and assigning the arguments
+            float[,] matrixA = new float[2, 2];
+            matrixA[0, 0] = float.Parse(args[0]);
+            matrixA[0, 1] = float.Parse(args[1]);
+            matrixA[1, 0] = float.Parse(args[2]);
+            matrixA[1, 1] = float.Parse(args[3]);
+
+            //Creating a new 2x1 matrix and assigning the arguments
+            float[,] matrixB = new float[2, 1];
+            matrixB[0, 0] = float.Parse(args[4]);
+            matrixB[1, 0] = float.Parse(args[5]);
         }
     }
 }
