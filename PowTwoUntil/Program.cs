fe18fb331fe1464aaeab74221ed26ec9c85fdeeb
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 17:29:50 +0000

    Created method PowerOf2Until5 and calling it twice on Main, PowTwoUntil project

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -6,7 +6,15 @@ namespace PowTwoUntil
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Calling PowersOf2Until5 method twice
+            PowersOf2Until5();
+            PowersOf2Until5();
+        }
+
+        private static void PowersOf2Until5(){
+            for (int i = 1; i <= (1 << 5); i = i << 1){
+                Console.WriteLine(i);
+            }
         }
     }
 }
