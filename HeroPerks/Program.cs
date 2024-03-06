9a89a26f6f5ce741848b660e5dd4f152bde03c48
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 16:51:01 +0000

    checking if input is unknown and dealing with unknown perks, HeroPerks project

--- HeroPerks/Program.cs
+++ HeroPerks/Program.cs
@@ -4,9 +4,33 @@ namespace HeroPerks
 {
     class Program
     {
+        [Flags]
+        enum Perks
+        {
+            WaterBreathing = 1,
+            Stealth = 1,
+            AutoHeal = 1,
+            DoubleJump = 1
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Perks perks;
+
+            string input = args[0];
+            bool unknown = false;
+
+            //Checking if all the inserted perks are known
+            foreach(char c in input){
+                if (c != 'w' && c != 'a' && c != 's' && c != 'd'){
+                    unknown = true;
+                }
+            }
+
+            //Dealing with unknown perks
+            if (unknown == true) {
+                Console.WriteLine("Unknown perk!");
+            }
         }
     }
 }
