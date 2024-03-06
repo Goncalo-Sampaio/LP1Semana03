8326c3e4c7ea5659d7f9365ed9be8e41b436ffa8
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 15:51:02 +0000

    Creating the for loops to iterate through the matrixes, ArrayMult project

--- ArrayMult/Program.cs
+++ ArrayMult/Program.cs
@@ -14,9 +14,23 @@ namespace ArrayMult
             matrixA[1, 1] = float.Parse(args[3]);
 
             //Creating a new 2x1 matrix and assigning the arguments
-            float[,] matrixB = new float[2, 1];
-            matrixB[0, 0] = float.Parse(args[4]);
-            matrixB[1, 0] = float.Parse(args[5]);
+            float[] matrixB = new float[2];
+            matrixB[0] = float.Parse(args[4]);
+            matrixB[1] = float.Parse(args[5]);
+
+            //Creating final matrix
+            float[] final = new float[2];
+
+            //double for loop to go through the multidimensional matrixes
+            for (int i = 0; i < matrixA.GetLength(0); i++){
+                float value = 0;
+                for ( int j = 0; j < matrixB.GetLength(1); j++){
+                    value += (matrixA[i, j] * matrixB[j]);
+                }
+
+                //assigning the value of the addition and multiplication
+                final[i] = value;
+            }
         }
     }
 }
