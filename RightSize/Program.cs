a21261886a6665bd85a583bbbdb397fc1306803c
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 13:07:48 +0000

    Added comments to RightSize project

--- RightSize/Program.cs
+++ RightSize/Program.cs
@@ -6,15 +6,19 @@ namespace RightSize
     {
         static void Main(string[] args)
         {
+            //Loop that will go through all the arguments used
             foreach (string s in args)
             {
+                //if the length of the argument is bigger than 3
                 if (s.Length > 3)
                 {
+                    //if the argument is bigger than 6, it breaks the loop
                     if (s.Length > 6)
                     {
                         break;
                     }
 
+                    //Writes all the arguments that have a length of 4-6
                     Console.WriteLine(s);
                 }
             }
