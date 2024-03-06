0eb02273b769c7087e7f23028833aacf0f177705
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 18:11:10 +0000

    Added method to print powers of 2 until a variable n, PowTwoUntil project

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -9,10 +9,19 @@ namespace PowTwoUntil
             //Calling PowersOf2Until5 method twice
             PowersOf2Until5();
             PowersOf2Until5();
+            //Calling PowersOf2UntilN method twice with different arguments
+            PowersOf2UntilN(0);
+            PowersOf2UntilN(7);
         }
 
+        //method that calls PowersOf2UntilN method with a n of 5
         private static void PowersOf2Until5(){
-            for (int i = 1; i <= (1 << 5); i = i << 1){
+            PowersOf2UntilN(5);
+        }
+
+        //method that prints powers of 2 until a certain number n
+        private static void PowersOf2UntilN(int n){
+            for (int i = 1; i <= (1 << n); i = i << 1){
                 Console.WriteLine(i);
             }
         }
