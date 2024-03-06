51133f52491f89bf5e6dad53390ee57a5d648604
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-06 18:15:47 +0000

    Added XML comments to the methods in PowTwoUntil project

--- PowTwoUntil/Program.cs
+++ PowTwoUntil/Program.cs
@@ -4,6 +4,10 @@ namespace PowTwoUntil
 {
     class Program
     {
+        /// <summary>
+        /// Main method where all the code will run.
+        /// </summary>
+        /// <param name="args">arguments written when running</param>
         static void Main(string[] args)
         {
             //Calling PowersOf2Until5 method twice
@@ -14,12 +18,17 @@ namespace PowTwoUntil
             PowersOf2UntilN(7);
         }
 
-        //method that calls PowersOf2UntilN method with a n of 5
+        /// <summary>
+        /// Method that calls PowersOf2UntilN method with a n of 5
+        /// </summary>
         private static void PowersOf2Until5(){
             PowersOf2UntilN(5);
         }
 
-        //method that prints powers of 2 until a certain number n
+        /// <summary>
+        /// Method that prints powers of 2 until a certain number n
+        /// </summary>
+        /// <param name="n"></param>
         private static void PowersOf2UntilN(int n){
             for (int i = 1; i <= (1 << n); i = i << 1){
                 Console.WriteLine(i);
