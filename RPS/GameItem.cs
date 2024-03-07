0fc3fa99a1b91dade6eeff1120fab75787b359e3
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-07 15:52:32 +0000

    Added 2 enum files to RPS project

---
+++ RPS/GameItem.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPS
+{
+    enum GameItem
+    {
+        Rock,
+        Paper,
+        Scissors
+    }
+}
---
+++ RPS/GameStatus.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RPS
+{
+    enum GameStatus
+    {
+        Draw, 
+        Player1Wins,
+        Player2Wins
+    }
+}
