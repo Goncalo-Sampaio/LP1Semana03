f65bc085d78ca7c13fd6c1d44fdfa7942e52afa4
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-03-08 17:06:47 +0000

    Added player input and parse to int on PlayerAchievements project

--- PlayerAchievements/Program.cs
+++ PlayerAchievements/Program.cs
@@ -6,7 +6,11 @@ namespace PlayerAchievements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Asking the user to insert number of players
+            Console.Write("Please insert the number of players: ");
+            string input = Console.ReadLine();
+            //saving the input on a int variable n
+            int n = int.Parse(input);
         }
     }
 }
